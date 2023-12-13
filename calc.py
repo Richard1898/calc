ae80aeef51442bb485179df06c14364da490aed5
Your Name <you@example.com>
2023-12-13 13:22:33 +0200

    Rihards

--- calc.py
+++ calc.py
@@ -1,22 +1,23 @@
 
 # konvertē datu mērvienības
 def megabyte_to_megabit(megabyte):
-   result = 0
-   return result
+    result = megabyte *8
+    return result
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
-   return result
+    result = (litres / kilometers)*100
+    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = celsius * 1.8 + 32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
-    result = 0
+
+    result = 
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
